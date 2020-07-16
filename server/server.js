577d68417f0824b83245bad615ef7888c5f679b4
Brian-McHugh <125cedarstreet@gmail.com>
2020-07-16 18:05:52 -0500

    Update server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = 5000;
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
-app.listen(1234, () => {
-  console.log(`hurray, you're connected!`);
+
+
+app.listen(port, () => {
+  console.log(`Nice job, you're connected!`);
 });
