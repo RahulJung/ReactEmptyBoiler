60ae4f93bb7fa99143f0c3047096f423c9de8725
Brian McHugh <125cedarstreet@gmail.com>
2020-07-18 17:32:26 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>ToDo List</h1> 
+        <span>Let's build a todo list!</span>
+      </div>
+    )
+  }
+}
+
+export default App;
