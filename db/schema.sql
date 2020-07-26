fbe30fb358272694fc646d9e1a20c2aab8693c43
Brian McHugh <125cedarstreet@gmail.com>
2020-07-26 17:00:41 -0500

    Correct syntax in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE tasks (
   task VARCHAR(255) NOT NULL
 );
 
-insert into tasks (task) values ('make todolist');
-insert into tasks (task) values ('learn react');
-insert into tasks (task) values ('get paid');
+INSERT INTO tasks (task) VALUES ('thank Ben for his help');
+INSERT INTO tasks (task) VALUES ('get React event handlers down');
+INSERT INTO tasks (task) VALUES ('ace the TA');
