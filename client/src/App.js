e91a67406b08dc94aec00bb1532a515a70372b8c
Brian McHugh <125cedarstreet@gmail.com>
2020-07-26 21:12:41 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,48 @@
 import React from 'react';
-import InputBar from './components/InputBar'
+import InputBar from './components/InputBar';
 import List from './components/List';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-      todolist: ['learn to code', 'make todolist', 'get paid'],
+      todolist: ['learn promises', 'make todolist', 'work out'],
       input: ''
-    }
+    };
     
     this.onInputChange = this.onInputChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.doneButtonClick = this.doneButtonClick.bind(this);
   }
 
-  onInputChange(event){
+  onInputChange(event) {
     this.setState({ 
-      input:event.target.value
-    })
+      input: event.target.value
+    });
   }
 
-  onSubmit(){
-    let newList = this.state.todolist.push(this.state.input)
+  onSubmit() {
+    let newList = this.state.todolist.push(this.state.input);
     this.setState({
       todoList: newList,
       input: ''
-    })
+    });
   }
 
-  doneButtonClick(e){
-    let newStateList = this.state.todolist.filter((item) => {
-      return item !== e.target.value
+  doneButtonClick(event) {
+    let newTaskList = this.state.todolist.filter((item) => {
+      return item !== event.target.value
     })
     this.setState({
-      todolist: newStateList
+      todolist: newTaskList
     })
   }
 
   render() {
     return (
       <div>
-        <h1>ToDo list</h1>
+        <h1>ToDo List</h1>
         <InputBar
           value={this.state.input}
           onInputChange={this.onInputChange}
