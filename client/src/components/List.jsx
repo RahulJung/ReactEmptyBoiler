c40d92ab1921d72b51c4da3e409c6db9eb291c72
Brian McHugh <125cedarstreet@gmail.com>
2020-07-26 21:15:45 -0500

    Update List.jsx

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-function List(props){
+function List(props) {
   return (
     <div className='task-list'>
       <ul>
         {props.todolist.map((task, index) => (
           <div key={index}>
-            <ListItem todoitem={task} 
-              doneButtonClick={props.doneButtonClick} />
+            <ListItem 
+              todoitem={task} 
+              doneButtonClick={props.doneButtonClick}
+            />
           </div>
         ))}
       </ul>
